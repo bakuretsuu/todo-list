e27f1a13acbc884da7ae1f8c844f7ee6d573dbdd
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-08-21 17:14:25 +0800

    create addtask and getuserinput function

---
+++ src/DOM.js
@@ -0,0 +1,10 @@
+export function getUserInput (){
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let dueDate = document.getElementById('dueDate').value;
+    let priority = document.getElementById('priority').checked;
+    let notes = document.getElementById('notes').value;
+    let id = crypto.randomUUID();
+
+    return {title, description, dueDate, priority, notes, id}
+}
--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,19 @@
-export function Tasks(title, description, dueDate, priority, notes, checklist) {
+export function Task(title, description, dueDate, priority, notes, id) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.notes = notes;
-    this.checklist = checklist;
+    this.id = id;
   }
+
+export function taskList(){
+  let myTasks = [];
+
+  const addTask = (taskData) => {
+    let newTask = new Task(taskData.title, taskData.description, taskData.dueDate, taskData.priority, taskData.notes, taskData.id);
+    myTasks.push(newTask);
+  }
+  return {addTask, myTasks};
+};
+
